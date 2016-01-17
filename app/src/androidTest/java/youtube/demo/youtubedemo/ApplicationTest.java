4d9227cf8fd756753ef9b71c64a5b517f0f5a69c
Lars Kniep <l.kniep@larscom.nl>
2016-01-17 18:02:47 +0100

    Initial Commit

---
+++ app/src/androidTest/java/youtube/demo/youtubedemo/ApplicationTest.java
@@ -0,0 +1,13 @@
+package youtube.demo.youtubedemo;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
